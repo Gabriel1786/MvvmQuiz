814c06c03e16ba7032beea5acf2233e64fba20a2
Gabriel den Hartog <gabriel.hartog@digitalpages.com.br>
2019-02-04 19:13:48 -0200

    setup UWP project

---
+++ MvvmQuiz.UWP/Setup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Forms.Platforms.Uap.Core;
+
+namespace MvvmQuiz.UWP
+{
+    public class Setup : MvxFormsWindowsSetup<Core.App, Forms.UI.App>
+    {
+    }
+}
